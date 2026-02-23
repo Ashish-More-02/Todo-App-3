f2550e19ea14f62c5d6c35e98a26b6846344546d
Ashish More <ashishmore2125@gmail.com>
2026-02-23 23:26:57 +0530

    React router setup with basic auth pages for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { NavLink } from "react-router";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,6 +8,17 @@ function App() {
   return (
     <div className="bg-gray-900 h-screen">
       <div className="text-4xl text-center text-white">Full stack - Todo App </div>
+
+      <button className="text-white rounded-2xl bg-teal-600 px-6 py-2"> 
+        <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >
+        Login
+      </NavLink>
+      </button>
     </div>
   );
 }
